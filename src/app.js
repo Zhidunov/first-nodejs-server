a3ebfc6f9e312c0ef3d273b5fb33e5a6f0a83f14
Zhidunov <36135311+Zhidunov@users.noreply.github.com>
2020-03-14 08:47:06 +0300

    Частичная реализация постраничного вывода.
    https://www.youtube.com/watch?v=ZX3qt0UWifc

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const app = express();
 
 app.use((req, res, next) => {
@@ -37,6 +36,87 @@ let hardRes = {
       name: "Сергей",
       status: "I-m the fullstack developer",
       location: { country: "Germany", city: "Berlin" }
+    },
+    {
+      id: 1,
+      avatarURL:
+        "https://previews.123rf.com/images/studiostoks/studiostoks1708/studiostoks170800016/84215159-surprise-woman-pop-art-avatar-character-icon.jpg",
+      followed: true,
+      name: "Дмитрий",
+      status: "I-m the frontend developer",
+      location: { country: "Russia", city: "Sochi" }
+    },
+    {
+      id: 2,
+      avatarURL:
+        "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/21514027/original/bf55eef1cfc1ba545ac9153cedebf61abe38af8a/create-your-pop-art-avatar-from-your-image.jpg",
+      followed: false,
+      name: "Алексей",
+      status: "I-m the backend developer",
+      location: { country: "Russia", city: "Saint-Petersburg" }
+    },
+    {
+      id: 3,
+      avatarURL:
+        "https://previews.123rf.com/images/studiostoks/studiostoks1707/studiostoks170700393/83085765-avatar-portrait-of-a-man-straightens-his-tie-pop-art-retro-vector-illustration.jpg",
+      followed: true,
+      name: "Сергей",
+      status: "I-m the fullstack developer",
+      location: { country: "Germany", city: "Berlin" }
+    },
+    {
+      id: 1,
+      avatarURL:
+        "https://previews.123rf.com/images/studiostoks/studiostoks1708/studiostoks170800016/84215159-surprise-woman-pop-art-avatar-character-icon.jpg",
+      followed: true,
+      name: "Дмитрий",
+      status: "I-m the frontend developer",
+      location: { country: "Russia", city: "Sochi" }
+    },
+    {
+      id: 2,
+      avatarURL:
+        "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/21514027/original/bf55eef1cfc1ba545ac9153cedebf61abe38af8a/create-your-pop-art-avatar-from-your-image.jpg",
+      followed: false,
+      name: "Алексей",
+      status: "I-m the backend developer",
+      location: { country: "Russia", city: "Saint-Petersburg" }
+    },
+    {
+      id: 3,
+      avatarURL:
+        "https://previews.123rf.com/images/studiostoks/studiostoks1707/studiostoks170700393/83085765-avatar-portrait-of-a-man-straightens-his-tie-pop-art-retro-vector-illustration.jpg",
+      followed: true,
+      name: "Сергей",
+      status: "I-m the fullstack developer",
+      location: { country: "Germany", city: "Berlin" }
+    },
+    {
+      id: 1,
+      avatarURL:
+        "https://previews.123rf.com/images/studiostoks/studiostoks1708/studiostoks170800016/84215159-surprise-woman-pop-art-avatar-character-icon.jpg",
+      followed: true,
+      name: "Дмитрий",
+      status: "I-m the frontend developer",
+      location: { country: "Russia", city: "Sochi" }
+    },
+    {
+      id: 2,
+      avatarURL:
+        "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/21514027/original/bf55eef1cfc1ba545ac9153cedebf61abe38af8a/create-your-pop-art-avatar-from-your-image.jpg",
+      followed: false,
+      name: "Алексей",
+      status: "I-m the backend developer",
+      location: { country: "Russia", city: "Saint-Petersburg" }
+    },
+    {
+      id: 3,
+      avatarURL:
+        "https://previews.123rf.com/images/studiostoks/studiostoks1707/studiostoks170700393/83085765-avatar-portrait-of-a-man-straightens-his-tie-pop-art-retro-vector-illustration.jpg",
+      followed: true,
+      name: "Сергей",
+      status: "I-m the fullstack developer",
+      location: { country: "Germany", city: "Berlin" }
     }
   ]
 };
@@ -44,7 +124,15 @@ let hardRes = {
 const port = 4000;
 
 app.get("/users", (req, res) => {
-  res.send(hardRes);
+  const page = req.query.page;
+  const count = req.query.count;
+
+  const startIndex = (page - 1) * count;
+  const endIndex = page * count;
+
+  const resultUsers = hardRes.users.slice(startIndex, endIndex);
+
+  res.json(resultUsers);
 });
 
 app.listen(port, () => {
