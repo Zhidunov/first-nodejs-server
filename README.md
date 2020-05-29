9fb5e7dfa03c2e901c2a545e587899d68cac9d5e
Кафка Тамура <mail.for.kafka@gmail.com>
2020-05-30 01:45:42 +0300

    Change CORS policy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# first-nodejs-server
-Created with CodeSandbox
+This is my first node.js app. It's simple API.
+
+Link on heroku: https://powerful-sands-35843.herokuapp.com
+
+API description will be added later.
--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 let hardRes = {
   users: [
